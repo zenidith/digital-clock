ca84d44f4d85efdee7d32c3260e7c53258f9f392
K Sheenan <kaynesheenan+git@gmail.com>
2021-08-21 17:25:29 +0900

    initiated the JS

---
+++ app.js
@@ -0,0 +1,11 @@
+function clock(){
+    var hours = document.getElementById('hour');
+    var minutes = document.getElementById('minutes');
+    var seconds = document.getElementById('seconds');
+
+    var h = new Date.UTC().getHours();
+    var m = new Date.UTC().getMinutes();
+    var s = new Date.UTC().getSeconds();
+
+}
+
